b0e414d587dadbb28f633c2f1b285db3372ae83e
concesar12 <concesar12@gmail.com>
2024-01-06 22:12:37 +1300

    New facade builder

--- Creational/Faceted-builder/Program.cs
+++ Creational/Faceted-builder/Program.cs
@@ -1,9 +1,117 @@
-﻿namespace Faceted_builder;
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
 
-class Program
+/*In this scenario things are a bit different, we were building from only one builder. However, this approach will bring us more builders 
+to create an object*/
+
+//Todo-Create Another builder that has eats and develop that.
+
+namespace Faceted_builder
 {
+public class Person
+  {
+    // address
+    public string StreetAddress, Postcode, City;
+
+    // employment
+    public string CompanyName, Position;
+
+    public int AnnualIncome;
+
+    public override string ToString()
+    {
+      return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+    }
+  }
+
+  public class PersonBuilder // facade 
+  {
+    // the object we're going to build
+    protected Person person = new Person(); // this is a reference!
+
+    public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+    public PersonJobBuilder Works => new PersonJobBuilder(person);
+    //This is to be able to print the person - Needs study
+    public static implicit operator Person(PersonBuilder pb)
+    {
+      return pb.person;
+    }
+  }
+
+  public class PersonJobBuilder : PersonBuilder
+  {
+    public PersonJobBuilder(Person person)
+    {
+      this.person = person;
+    }
+
+    public PersonJobBuilder At(string companyName)
+    {
+      person.CompanyName = companyName;
+      return this;
+    }
+
+    public PersonJobBuilder AsA(string position)
+    {
+      person.Position = position;
+      return this;
+    }
+
+    public PersonJobBuilder Earning(int annualIncome)
+    {
+      person.AnnualIncome = annualIncome;
+      return this;
+    }
+  }
+
+  public class PersonAddressBuilder : PersonBuilder
+  {
+    // might not work with a value type!
+    public PersonAddressBuilder(Person person)
+    {
+      this.person = person;
+    }
+
+    public PersonAddressBuilder At(string streetAddress)
+    {
+      person.StreetAddress = streetAddress;
+      return this;
+    }
+
+    public PersonAddressBuilder WithPostcode(string postcode)
+    {
+      person.Postcode = postcode;
+      return this;
+    }
+
+    public PersonAddressBuilder In(string city)
+    {
+      person.City = city;
+      return this;
+    }
+    
+  }
+
+  public class Demo
+  {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+      var pb = new PersonBuilder();
+      Person person = pb
+        .Lives
+          .At("123 London Road")
+          .In("London")
+          .WithPostcode("SW12BC")
+        .Works
+          .At("Fabrikam")
+          .AsA("Engineer")
+          .Earning(123000);
+
+      WriteLine(person);
     }
+  }
 }
