5f606973107ccb3050df78b67ed86ddf686efc57
concesar12 <cesar-guerrero@javeriana.edu.co>
2024-01-07 19:43:50 +1300

    Coding exercise

--- Creational/Builder/Program.cs
+++ Creational/Builder/Program.cs
@@ -106,36 +106,35 @@ namespace Builder
     static void Main(string[] args)
     {
       // if you want to build a simple HTML paragraph using StringBuilder
-      var hello = "hello";
+      // var hello = "hello";
       var sb = new StringBuilder();
-      sb.Append("<p>");
-      sb.Append(hello);
-      sb.Append("</p>");
-      WriteLine(sb);
+      // sb.Append("<p>");
+      // sb.Append(hello);
+      // sb.Append("</p>");
+      // WriteLine(sb);
 
-      // now I want an HTML list with 2 words in it
-      var words = new[] {"hello", "world"};
-      sb.Clear();
-      sb.Append("<ul>");
-      foreach (var word in words)
-      {
-        sb.AppendFormat("<li>{0}</li>", word);
-      }
-      sb.Append("</ul>");
-      WriteLine(sb);
+      // // now I want an HTML list with 2 words in it
+      // var words = new[] {"hello", "world"};
+      // sb.Clear();
+      // sb.Append("<ul>");
+      // foreach (var word in words)
+      // {
+      //   sb.AppendFormat("<li>{0}</li>", word);
+      // }
+      // sb.Append("</ul>");
+      // WriteLine(sb);
 
 //------------Here is where the actual pattern is applied--------
 
       // ordinary non-fluent builder
-      var builder = new HtmlBuilder("ul");
-      builder.AddChild("li", "hello");
-      builder.AddChild("li", "world");
-      WriteLine(builder.ToString());
+      // builder.AddChild("li", "hello");
+      // builder.AddChild("li", "world");
+      // WriteLine(builder.ToString());
 
-      // fluent builder -> with fluent we just return the object straight 
-      sb.Clear();
-      builder.Clear(); // disengage builder from the object it's building, then...
-      builder.AddChildFluent("li", "hello").AddChildFluent("li", "world");
+      // // fluent builder -> with fluent we just return the object straight 
+      // sb.Clear();
+      // builder.Clear(); // disengage builder from the object it's building, then...
+      var builder = new HtmlBuilder("ul").AddChildFluent("li", "hello").AddChildFluent("li", "world");
       WriteLine(builder);
     }
   }
---
+++ Creational/Builder/code-builder-quiz.cs
@@ -0,0 +1,28 @@
+/*Considerations for coding exercise
+1. The root of the code chain looks is "public class"
+2. After the root is given with the name of the parameter then a curly bracket is given next
+3. Looks like root is always public.
+4. only if the in add field we add the name and the type
+5. after each add field we place a semicolon ";"
+6. we finish with a curly braces
+
+Algorithm
+1. string root
+2. dictionary field
+3. class CodeBuilder 
+
+root = "public"
+
+
+*/
+
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class CodeBuilder
+    {
+        //
+    }
+}
